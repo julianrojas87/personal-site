e1ebb319c57745db79e4676a3a19f72750b851ec
Julian Rojas <julian.rojas87@gmail.com>
2019-10-02 02:17:42 +0200

    Remove unnecessary stuff

--- eleventy/js/main.js
+++ eleventy/js/main.js
@@ -7,16 +7,6 @@ AOS.init({
 
 	"use strict";
 
-	$(window).stellar({
-		responsive: true,
-		parallaxBackgrounds: true,
-		parallaxElements: true,
-		horizontalScrolling: false,
-		hideDistantElements: false,
-		scrollProperty: 'scroll'
-	});
-
-
 	var fullHeight = function () {
 
 		$('.js-fullheight').css('height', $(window).height());
